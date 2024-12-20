f478d7bea60d4005afcd1a5b7dcc1cac1c7b205d
Grégoire <gbadiche@gmail.com>
2024-12-20 19:33:19 +0100

    building the base shell

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,57 @@
+'use strict';
+
+import { elements } from "./modules/heimdall.js";
+
 const main = () => {
-    var map = L.map('map').setView([48.8553633, 2.3449551], 13);
-    
+    var map = L.map('map', {
+        attributionControl: false,
+        zoomControl: false,
+    }).setView([48.8553633, 2.3449551], 13);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-}
+
+    L.control.attribution({
+        position: 'topright'
+    }).addTo(map);
+    
+    L.control.zoom({
+        position: 'topright'
+    }).addTo(map);
+
+    map.on('mousemove', (e) => {
+        var lat = e.latlng.lat.toString().slice(0, 10);
+        var lng = e.latlng.lng.toString().slice(0, 10);
+        elements.coordinates.innerText = `${lat}, ${lng}`;
+    });
+
+    let isResizing = false;
+
+    elements.appResize.addEventListener("mousedown", (e) => {
+        isResizing = true;
+
+        const startWidth = elements.app.offsetWidth;
+        const startX = e.clientX;
+
+        const onMouseMove = (event) => {
+            if (!isResizing) return;
+
+            const newWidth = startWidth + (event.clientX - startX);
+
+            elements.app.style.width = `${newWidth}px`;
+        };
+
+        const onMouseUp = () => {
+            isResizing = false;
+            window.removeEventListener('mousemove', onMouseMove);
+            window.removeEventListener('mouseup', onMouseUp);
+        };
+
+        window.addEventListener('mousemove', onMouseMove);
+        window.addEventListener('mouseup', onMouseUp);
+    })
+};
 
 window.onload = main;
---
+++ scripts/modules/heimdall.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const elements = {
+    app: document.getElementById("app"),
+    appResize: document.getElementById("app-resize"),
+    coordinates: document.getElementById("app-coordinates"),
+};
+
+export { elements };
